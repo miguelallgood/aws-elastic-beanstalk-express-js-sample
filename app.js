884334e2244583de8126ddeff5b56f8569f8f2f1
Miguel Bueno <bueno.reyes.miguel@gmail.com>
2024-06-21 12:16:26 +0200

    modified welcome message to Vanessa

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Miguel, I wish you a nice weekend!'));
+app.get('/', (req, res) => res.send('Hello Vanessa, I wish you a nice weekend ahead!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
